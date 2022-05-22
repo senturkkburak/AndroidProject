1281f636a6c0bd402ca0a7ac6ea39c4d5e4e6a11
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2022-05-22 17:15:45 +0300

    new uı

---
+++ app/src/main/java/com/example/androidproject/clocksFour.java
@@ -0,0 +1,14 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clocksFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clocks_four);
+    }
+}
