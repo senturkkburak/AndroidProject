1620f152e068c122f944b45e4f203fb564843c4d
BURAK SENTURK <burak.senturk@isik.edu.tr>
2022-06-06 00:00:57 +0300

    Clock Sound Buttons Added

--- app/src/main/java/com/example/androidproject/clocksOne.java
+++ app/src/main/java/com/example/androidproject/clocksOne.java
@@ -3,6 +3,7 @@ package com.example.androidproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ public class clocksOne extends AppCompatActivity {
     private Button goLearn;
     private Button backOne;
     private Button goOne;
-
+    private Button zeroSound,oneSound,twoSound,threeSound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,10 @@ public class clocksOne extends AppCompatActivity {
         goLearn = findViewById(R.id.menu);
         backOne = findViewById(R.id.backone);
         goOne = findViewById(R.id.goOne);
+        zeroSound=findViewById(R.id.clockZeroSound);
+        oneSound=findViewById(R.id.clockOneSound);
+        twoSound=findViewById(R.id.clockTwoSound);
+        threeSound=findViewById(R.id.clockThreeSound);
 
         goLearn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,5 +44,34 @@ public class clocksOne extends AppCompatActivity {
                 startActivity(new Intent(clocksOne.this,activityClocksTwo.class));
             }
         });
+        zeroSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(clocksOne.this,R.raw.zyzz);
+                mediaPlayer.start();
+            }
+        });
+        oneSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(clocksOne.this,R.raw.zyzz);
+                mediaPlayer.start();
+            }
+        });
+        twoSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(clocksOne.this,R.raw.zyzz);
+                mediaPlayer.start();
+            }
+        });
+        threeSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final MediaPlayer mediaPlayer = MediaPlayer.create(clocksOne.this,R.raw.zyzz);
+                mediaPlayer.start();
+            }
+        });
+
     }
 }
