9656e36bec1836d96d1b545c243d3e84ec7e9716
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2022-06-06 00:10:11 +0300

    digits

--- app/src/main/java/com/example/androidproject/digits.java
+++ app/src/main/java/com/example/androidproject/digits.java
@@ -2,13 +2,41 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class digits extends AppCompatActivity {
+    private Button goMenu;
+    private Button back;
+    private Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_digits);
+
+        goMenu = findViewById(R.id.digitsMenu);
+        back = findViewById(R.id.digitsBack);
+        go = findViewById(R.id.digitsGo);
+        goMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(digits.this,learn.class));
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(digits.this,learn.class));
+            }
+        });
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(digits.this,learn.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/androidproject/learn.java
+++ app/src/main/java/com/example/androidproject/learn.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class learn extends AppCompatActivity {
     private Button clockButton;
     private Button backButton;
+    private Button digitsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +19,24 @@ public class learn extends AppCompatActivity {
 
         clockButton = findViewById(R.id.clock);
         backButton = findViewById(R.id.back);
+        digitsButton = findViewById(R.id.digits);
 
         clockButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(learn.this,clocksOne.class));
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(learn.this,home.class));
+            }
+        });
+        digitsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(learn.this,digits.class));
             }
         });
     }
