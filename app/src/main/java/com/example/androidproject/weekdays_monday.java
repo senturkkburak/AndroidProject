f5a627f1a9f34dc4d90f5adbcc4e5857abd4b97f
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2022-06-14 01:38:43 +0300

    play ground added

---
+++ app/src/main/java/com/example/androidproject/weekdays_monday.java
@@ -0,0 +1,14 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weekdays_monday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekdays_monday);
+    }
+}
